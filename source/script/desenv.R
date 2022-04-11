e87f145c48a0c56345f1e6e74f01ef1ede77ffab
luan <pauloluan09@gmail.com>
2022-04-10 22:55:37 -0300

    #2

--- source/script/desenv.R
+++ source/script/desenv.R
@@ -4,7 +4,7 @@
 rm(list = ls())
 
 #INSTALACAO E/OU CARREGAMENTO DOS PACOTES
-pacotes = c("dplyr", "lubridate", "stringr", "tictoc", "ggplot2", "tidymodels", "ranger", "doSNOW", "xgboost", "abjutils", "forcats")
+pacotes = c("dplyr", "lubridate", "stringr", "tictoc", "ggplot2", "ggeasy", "ggpubr", "tidymodels", "ranger", "doSNOW", "xgboost", "abjutils", "forcats")
 novos.pacotes = pacotes[!(pacotes %in% installed.packages()[, "Package"])]
 if(length(novos.pacotes)) install.packages(novos.pacotes, repos = 'https://cran.us.r-project.org')
 options(warn = -1)
@@ -97,40 +97,294 @@ data = orders %>%
 #REMOVENDO AS BASES QUE NAO SERAO MAIS UTILIZADAS
 remove(order_items, order_payments, order_reviews, products, customers, customers_idh, sellers, sellers_idh, orders, geolocation, idh_atlas, data_idh, idh, uf)
 
+
 # ANALISE EXPLORATORIA ------------------------------------------------------------
 
+#CONTAGEM DE LINHAS COM VALORES NULOS
+countNA = function(x) sum(is.na(x)) 
+
+rows_with_na = data %>%
+  dplyr::mutate(flag_na = as.factor(dplyr::case_when(apply(data, 1, countNA) == 0 ~ 0, TRUE ~ 1))) %>%
+  dplyr::group_by(flag_na) %>%
+  dplyr::summarise(na = dplyr::n(), prop = round(na/nrow(data) * 100, 2)) %>%
+  dplyr::mutate(value = paste0(format(na, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)"))
+
+#GRAFICO PARA APRESENTAR A QTDE DE LINHAS COM VALORES NULOS
+plot_na = ggplot(data=rows_with_na, aes(x = flag_na, y = na)) +
+  geom_bar(stat = "identity", fill = "steelblue")+
+  geom_text(aes(label = value), vjust = -0.3, size = 3.5)+
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  xlab("Nulos") +
+  ylab("Quantidade") +
+  ggtitle(label = "Linhas sem Valores Nulos x Com valores Nulos") +
+  ggeasy::easy_center_title()
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/rows_with_na.png", plot = plot_na, height = 5, width = 5.5)
+
+#QUANTIDADE POR REVIEW SCORE
+review_score = data %>%
+  dplyr::group_by(review_score) %>%
+  dplyr::summarise(score = dplyr::n(),
+                   prop = round(score/nrow(data) * 100, 2)) %>%
+  dplyr::mutate(value = paste0(format(score, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)"))
+
+#GRAFICO PARA APRESENTAR A DISTRIBUICAO DE AVALIACOES POR SCORE DA POPULACAO
+plot_review_score = ggplot(data=review_score, aes(x = review_score, y = score)) +
+  geom_bar(stat = "identity", fill = "steelblue")+
+  geom_text(aes(label = value), vjust = -0.3, size = 3.5)+
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  xlab("Nota (Score)") +
+  ylab("Quantidade") +
+  ggtitle(label = "Reviews por Nota (Score)") +
+  ggeasy::easy_center_title()
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/review_score.png", plot = plot_review_score, height = 5, width = 5.5)
+
 #VERIFICANDO BALANCEAMENTO DA POPULACAO
-avaliacao = data %>%
+balance = data %>%
   dplyr::group_by(target) %>%
   dplyr::summarise(reviews = dplyr::n(),
                    prop = round(reviews/nrow(data) * 100, 2)) %>%
-  dplyr::mutate(value = paste0(reviews, " (", format(prop, decimal.mark = ','), "%)"))
+  dplyr::mutate(value = paste0(format(reviews, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)"))
 
 #GRAFICO PARA APRESENTAR O BALANCO DA POPULACAO
-plot_avaliacao = ggplot(data=avaliacao, aes(x = target, y = reviews)) +
+plot_balance = ggplot(data=avaliacao, aes(x = target, y = reviews)) +
   geom_bar(stat = "identity", fill = "steelblue")+
   geom_text(aes(label = value), vjust = -0.3, size = 3.5)+
-  theme_minimal()
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  xlab("Target") +
+  ylab("Quantidade") +
+  ggtitle(label = "Reviews (Bons x Ruins)") +
+  ggeasy::easy_center_title()
 
 #SALVANDO ARQUIVO DO PLOT
-ggsave(filename = "plots/review_balance.png", plot = plot_avaliacao)
+ggsave(filename = "plots/review_balance.png", plot = plot_balance, height = 5, width = 5.5)
 
-#QUANTIDADE DE REVIEWS POR MES
-reviews_mes = data %>%
-  dplyr::group_by(yearmon) %>%
+#QUANTIDADE DE REVIEWS POR REGIAO (CLIENTE)
+reviews_regiao_cli = data %>%
+  dplyr::mutate(reg_cli = dplyr::case_when(customer_state %in% c('AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO') ~ 'Norte', 
+                                           customer_state %in% c('AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'SE', 'RN') ~ 'Nordeste', 
+                                           customer_state %in% c('DF', 'GO', 'MS', 'MT') ~ 'Centro-Oeste',
+                                           customer_state %in% c('ES', 'MG', 'RJ', 'SP') ~ 'Sudeste', 
+                                           customer_state %in% c('PR', 'SC', 'RS') ~ 'Sul', 
+                                           TRUE ~ 'Não Consta')) %>%
+  dplyr::group_by(reg_cli) %>%
   dplyr::summarise(reviews = dplyr::n(),
                    prop = round(reviews/nrow(data) * 100, 2)) %>%
-  dplyr::mutate(value = paste0(reviews, " (", format(prop, decimal.mark = ','), "%)"))
+  dplyr::mutate(value = paste0(format(reviews, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)")) %>%
+  dplyr::filter(reg_cli != "Não Consta")
 
-#GRAFICO DE REVIEWS POR MES
-plot_reviews_mes = ggplot(data=reviews_mes, aes(x = yearmon, y = reviews, group = 1)) +
-  geom_line(fill = "steelblue")+
-  geom_point()+
-  geom_text(aes(label = reviews), vjust = -0.6, size = 3.5)+
-  theme_minimal()
+#GRAFICO PARA APRESENTAR OS REVIEWS POR REGIAO (CLIENTE)
+plot_regiao_cli = ggplot(data=reviews_regiao_cli, aes(x = reg_cli, y = reviews)) +
+  geom_bar(stat = "identity", position = 'identity', fill = "steelblue")+
+  geom_text(aes(label = value), vjust = -0.3, size = 3.5)+
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Reviews por Região do Cliente") +
+  xlab("Região") + 
+  ylab("Quantidade") +
+  ggeasy::easy_center_title()
 
 #SALVANDO ARQUIVO DO PLOT
-ggsave(filename = "plots/review_mes.png", plot = plot_reviews_mes)
+ggsave(filename = "plots/reviews_regiao_cli.png", plot = plot_regiao_cli, height = 5, width = 5.5)
+
+#QUANTIDADE REVIEWS POR REGIAO E AVALIACAO
+reviews_regiao_cli_target = data %>%
+  dplyr::mutate(reg_cli = dplyr::case_when(customer_state %in% c('AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO') ~ 'Norte', 
+                                           customer_state %in% c('AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'SE', 'RN') ~ 'Nordeste', 
+                                           customer_state %in% c('DF', 'GO', 'MS', 'MT') ~ 'Centro-Oeste',
+                                           customer_state %in% c('ES', 'MG', 'RJ', 'SP') ~ 'Sudeste', 
+                                           customer_state %in% c('PR', 'SC', 'RS') ~ 'Sul', 
+                                           TRUE ~ 'Não Consta')) %>%
+  dplyr::group_by(reg_cli, target) %>%
+  dplyr::summarise(reviews = dplyr::n()) %>%
+  dplyr::mutate(prop = round(reviews/sum(reviews) * 100, 2), 
+                value = paste0(format(reviews, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)")) %>%
+  dplyr::filter(reg_cli != "Não Consta")
+
+#GRAFICO PARA APRESENTAR OS REVIEWS POR REGIAO (CLIENTE)
+plot_regiao_cli_target = ggplot(data = reviews_regiao_cli_target, aes(x = reg_cli, y = prop, fill = target)) +
+  geom_bar(position = "fill", stat = "identity")+
+  geom_text(aes(label = value), position = "fill", vjust = -0.3, size = 2.5) +
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Reviews por Região do Cliente e suas Avaliações") +
+  xlab("Região") + 
+  ylab("Proporção") +
+  ggeasy::easy_center_title()
+  
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/reviews_regiao_cli_target.png", plot = plot_regiao_cli_target, height = 5, width = 5.5)
+
+#QUANTIDADE DE REVIEWS POR REGIAO (VENDEDOR)
+reviews_regiao_vend = data %>%
+  dplyr::mutate(reg_vend = dplyr::case_when(seller_state %in% c('AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO') ~ 'Norte',
+                                            seller_state %in% c('AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'SE', 'RN') ~ 'Nordeste',
+                                            seller_state %in% c('DF', 'GO', 'MS', 'MT') ~ 'Centro-Oeste',
+                                            seller_state %in% c('ES', 'MG', 'RJ', 'SP') ~ 'Sudeste',
+                                            seller_state %in% c('PR', 'SC', 'RS') ~ 'Sul',
+                                            TRUE ~ 'Não Consta')) %>%
+  dplyr::group_by(reg_vend) %>%
+  dplyr::summarise(reviews = dplyr::n(),
+                   prop = round(reviews/nrow(data) * 100, 2)) %>%
+  dplyr::mutate(value = paste0(format(reviews, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)")) %>%
+  dplyr::filter(reg_vend != "Não Consta")
+
+#GRAFICO PARA APRESENTAR OS REVIEWS POR REGIAO (VENDEDOR)
+plot_regiao_vend = ggplot(data=reviews_regiao_vend, aes(x = reg_vend, y = reviews)) +
+  geom_bar(stat = "identity", fill = "steelblue")+
+  geom_text(aes(label = value), vjust = -0.3, size = 3.5)+
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Reviews por Região do Vendedor") +
+  xlab("Região") + 
+  ylab("Quantidade") +
+  ggeasy::easy_center_title()
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/reviews_regiao_vend.png", plot = plot_regiao_vend, height = 5, width = 5.5)
+
+#QUANTIDADE REVIEWS POR REGIAO E AVALIACAO
+reviews_regiao_vend_target = data %>%
+  dplyr::mutate(reg_vend = dplyr::case_when(seller_state %in% c('AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO') ~ 'Norte',
+                                            seller_state %in% c('AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'SE', 'RN') ~ 'Nordeste',
+                                            seller_state %in% c('DF', 'GO', 'MS', 'MT') ~ 'Centro-Oeste',
+                                            seller_state %in% c('ES', 'MG', 'RJ', 'SP') ~ 'Sudeste',
+                                            seller_state %in% c('PR', 'SC', 'RS') ~ 'Sul',
+                                            TRUE ~ 'Não Consta')) %>%
+  dplyr::group_by(reg_vend, target) %>%
+  dplyr::summarise(reviews = dplyr::n()) %>%
+  dplyr::mutate(prop = round(reviews/sum(reviews) * 100, 2), 
+                value = paste0(format(reviews, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)")) %>%
+  dplyr::filter(reg_vend != "Não Consta")
+
+#GRAFICO PARA APRESENTAR OS REVIEWS POR REGIAO (VENEDOR E TARGET)
+plot_regiao_vend_target = ggplot(data = reviews_regiao_vend_target, aes(x = reg_vend, y = prop, fill = target)) +
+  geom_bar(position = "fill", stat = "identity")+
+  geom_text(aes(label = value), position = "fill", vjust = -0.3, size = 2.5) +
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Reviews por Região do Vendedor e suas Avaliações") +
+  xlab("Região") + 
+  ylab("Porcentagem") +
+  ggeasy::easy_center_title()
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/reviews_regiao_vend_target.png", plot = plot_regiao_vend_target, height = 5, width = 5.5)
+
+#QUANTIDADE DE REVIEWS EM QUE A REGIAO DO VENDEDOR E A MESMA DO CLIENTE
+reviews_regiao_vend_cli_target = data %>%
+  dplyr::mutate(reg_vend = dplyr::case_when(seller_state %in% c('AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO') ~ 'Norte',
+                                            seller_state %in% c('AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'SE', 'RN') ~ 'Nordeste',
+                                            seller_state %in% c('DF', 'GO', 'MS', 'MT') ~ 'Centro-Oeste',
+                                            seller_state %in% c('ES', 'MG', 'RJ', 'SP') ~ 'Sudeste',
+                                            seller_state %in% c('PR', 'SC', 'RS') ~ 'Sul',
+                                            TRUE ~ 'Não Consta'), 
+                reg_cli = dplyr::case_when(customer_state %in% c('AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO') ~ 'Norte', 
+                                           customer_state %in% c('AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'SE', 'RN') ~ 'Nordeste', 
+                                           customer_state %in% c('DF', 'GO', 'MS', 'MT') ~ 'Centro-Oeste',
+                                           customer_state %in% c('ES', 'MG', 'RJ', 'SP') ~ 'Sudeste', 
+                                           customer_state %in% c('PR', 'SC', 'RS') ~ 'Sul', 
+                                           TRUE ~ 'Não Consta'), 
+                reg_vend_cli = ifelse(test = reg_vend == reg_cli, yes = 'Mesma Região', no = 'Regiões Diferentes')) %>%
+  dplyr::group_by(reg_vend_cli, target) %>%
+  dplyr::summarise(reviews = dplyr::n()) %>%
+  dplyr::mutate(prop = round(reviews/sum(reviews) * 100, 2), 
+                value = paste0(format(reviews, big.mark = '.'), " (", format(prop, decimal.mark = ','), "%)"))
+
+#GRAFICO PARA APRESENTAR OS REVIEWS POR REGIAO IGUAL ENTRE CLIENTES E VENDEDORES
+plot_regiao_vend_cli_target = ggplot(data=reviews_regiao_vend_cli, aes(x = reg_vend_cli, y = reviews, fill = target)) +
+  geom_bar(stat = "identity", position = "fill")+
+  geom_text(aes(label = value), vjust = -0.3, size = 3.5, position = "fill")+
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Reviews por Igualdade de Região (Cliente e Vendedor) \n e suas Avaliações") +
+  xlab("Região") +
+  ylab("Proporção") +
+  ggeasy::easy_center_title()
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/reviews_regiao_vend_cli_target.png", plot = plot_regiao_vend_cli_target, height = 5, width = 5.5)
+
+#IDH CLIENTE POR AVALIACAO
+plot_idh_cli_target = ggplot(data = data, aes(x=target, y=customer_idhm)) + 
+  geom_boxplot(fill = "steelblue") +
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Distribuição de IDH por \n Cliente e Avaliação") +
+  xlab("Target") +
+  ylab("IDH") +
+  ggeasy::easy_center_title()
+
+#IDH VENDEDOR POR AVALIACAO
+plot_idh_vend_target = ggplot(data = data, aes(x=target, y=seller_idhm)) + 
+  geom_boxplot(fill = "steelblue") +
+  theme(panel.border = element_rect(color = "steelblue",
+                                    fill = NA,
+                                    size = 2)) +
+  ggtitle(label = "Distribuição de IDH por \n Vendedor e Avaliação") +
+  xlab("Target") +
+  ylab("IDH") +
+  ggeasy::easy_center_title()
+
+#PLOTANDO OS GRAFICOS JUNTOS
+plot_idh_vend_cli_target = ggpubr::ggarrange(plot_idh_cli_target, plot_idh_vend_target, 
+          ncol = 2, 
+          nrow = 1, 
+          vjust = 2, 
+          hjust = 0)
+
+#SALVANDO ARQUIVO DO PLOT
+ggsave(filename = "plots/idh_vend_cli_target.png", plot = plot_idh_vend_cli_target, height = 5, width = 5.5)
+
+#QUANTIDADE POR GRUPOS DE PRODUTOS
+tipo_produto_target = data %>%
+  dplyr::group_by(product_category_name) %>%
+  dplyr::summarise(n = dplyr::n()) %>%
+  dplyr::arrange(desc(n)) %>%
+  dplyr::mutate(grupo_produto = ifelse(test = n >= 2400, yes = product_category_name, no = "outros"))
+
+ggplot(data = tipo_produto_target, aes(qt, colour = product_category_name)) + 
+stat_ecdf(geom = "step")
+#sum(tipo_produto_target[1:16, 2])/sum(tipo_produto_target[, 2])
+
+# #QUANTIDADE DE REVIEWS POR MES
+# reviews_mes = data %>%
+#   dplyr::group_by(yearmon) %>%
+#   dplyr::summarise(reviews = dplyr::n(),
+#                    prop = round(reviews/nrow(data) * 100, 2)) %>%
+#   dplyr::mutate(value = paste0(reviews, " (", format(prop, decimal.mark = ','), "%)"))
+# 
+# #GRAFICO DE REVIEWS POR MES
+# plot_reviews_mes = ggplot(data=reviews_mes, aes(x = yearmon, y = reviews, group = 1)) +
+#   geom_line(fill = "steelblue")+
+#   geom_text(aes(label = value), vjust = -0.3, size = 3.5, position = "identity")+
+#   theme(panel.border = element_rect(color = "steelblue",
+#                                     fill = NA,
+#                                     size = 2)) +
+#   ggtitle(label = "Reviews por Mês") +
+#   xlab("Ano/Mês") +
+#   ylab("Quantidade") +
+#   ggeasy::easy_center_title()
+# 
+# 
+# #SALVANDO ARQUIVO DO PLOT
+# ggsave(filename = "plots/review_mes.png", plot = plot_reviews_mes)
 
 # FEATURE ENGINEERING -----------------------------------------------------
 
